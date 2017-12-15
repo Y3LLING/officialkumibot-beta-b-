eaa04f261b88eb39f7fb9b997f405b72e9525bf7
Y3LLING <34530438+Y3LLING@users.noreply.github.com>
2017-12-14 21:10:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-client.login(process.env.BOT_TOKEN);
-
-
 var Discord = require('discord.js');
 var Bot = new Discord.Client();
 var Helper = require('./components/helper.js');
